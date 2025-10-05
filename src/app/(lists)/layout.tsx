fa1478efa52b4c795b1896753c6b204aa0ae94cf
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-10-05 10:52:19 +0200

    fix header height

--- src/app/(lists)/layout.tsx
+++ src/app/(lists)/layout.tsx
@@ -6,7 +6,7 @@ export default function ListLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="min-h-screen flex flex-col font-sans">
+    <div className="flex flex-col font-sans">
       <ListHeader />
       {children}
     </div>
